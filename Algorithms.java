be348f7d3adb66f46823862af5bbc73b07bc4575
michael-torkaman <michael.torkaman.t@gmail.com>
2023-06-25 21:59:23 -0700

    prototype of how the Algorithms class should work

--- Algorithms.java
+++ Algorithms.java
@@ -1,3 +1,52 @@
 public class Algorithms {
+
     
+    public static void InsertionSort(int[] arr, int delay, Visualizer visualizer){
+     
+        int n = arr.length;
+        for (int i = 1; i < n; ++i) {
+            int key = arr[i];
+            int j = i - 1;
+            
+            visualizer.setArr(arr);
+            visualizer.setComparedIndxes(key, j);
+            visualizer.repaint();
+            
+            try {
+                Thread.sleep(delay); 
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+
+            while (j >= 0 && arr[j] > key) {
+                arr[j + 1] = arr[j];
+                j = j - 1;
+
+
+            visualizer.setArr(arr);
+            visualizer.setComparedIndxes(key, j);
+            visualizer.repaint();
+            
+            try {
+                Thread.sleep(delay); 
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+
+
+            }
+
+            arr[j + 1] = key;
+
+            visualizer.setArr(arr);
+            visualizer.setComparedIndxes(key, j);
+            visualizer.repaint();
+            
+            try {
+                Thread.sleep(delay); 
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+    }
 }
