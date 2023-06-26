163941c39e14ea3e9261e617471f815111ec48a4
michael-torkaman <michael.torkaman.t@gmail.com>
2023-06-26 00:23:23 -0700

    driver code and more Algorithms implemented

--- Algorithms.java
+++ Algorithms.java
@@ -9,7 +9,7 @@ public class Algorithms {
             int j = i - 1;
             
             visualizer.setArr(arr);
-            visualizer.setComparedIndxes(key, j);
+            visualizer.setComparedIndxes(i, j);
             visualizer.repaint();
             
             try {
@@ -24,7 +24,7 @@ public class Algorithms {
 
 
             visualizer.setArr(arr);
-            visualizer.setComparedIndxes(key, j);
+            visualizer.setComparedIndxes(i, j);
             visualizer.repaint();
             
             try {
@@ -33,13 +33,12 @@ public class Algorithms {
                 e.printStackTrace();
             }
 
-
             }
 
             arr[j + 1] = key;
 
             visualizer.setArr(arr);
-            visualizer.setComparedIndxes(key, j);
+            visualizer.setComparedIndxes(i, j);
             visualizer.repaint();
             
             try {
@@ -48,5 +47,111 @@ public class Algorithms {
                 e.printStackTrace();
             }
         }
+    }
+     public static void mergeSort(int[] arr, int delay, Visualizer visualizer) {
+        if (arr.length <= 1) {
+            return;
+        }
+
+        int mid = arr.length / 2;
+        int[] left = new int[mid];
+        int[] right = new int[arr.length - mid];
+        System.arraycopy(arr, 0, left, 0, mid);
+        System.arraycopy(arr, mid, right, 0, arr.length - mid);
+
+        mergeSort(left, delay, visualizer);
+        mergeSort(right, delay, visualizer);
+
+        merge(arr, left, right, delay, visualizer);
+
+    }
+    private static void merge(int[] arr, int[] left, int[] right, int delay, Visualizer visualizer) {
+        int leftSize = left.length;
+        int rightSize = right.length;
+        int i = 0, j = 0, k = 0;
+
+        while (i < leftSize && j < rightSize) {
+            if (left[i] <= right[j]) {
+                arr[k++] = left[i++];
+            } else {
+                arr[k++] = right[j++];
+            }
+
+            visualizer.setArr(arr);
+            visualizer.setComparedIndxes(i + j - 1, k - 1);
+            visualizer.repaint();
+
+            try {
+                Thread.sleep(delay);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+
+        while (i < leftSize) {
+            arr[k++] = left[i++];
+
+            visualizer.setArr(arr);
+            visualizer.setComparedIndxes(i + rightSize - 1, k - 1);
+            visualizer.repaint();
+
+            try {
+                Thread.sleep(delay);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+
+        while (j < rightSize) {
+            arr[k++] = right[j++];
+
+            visualizer.setArr(arr);
+            visualizer.setComparedIndxes(i + j - 1, k - 1);
+            visualizer.repaint();
+            try {
+                Thread.sleep(delay);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    public static void selectionSort(int[] arr, int delay, Visualizer visualizer){
+        int n = arr.length;
+ 
+        // One by one move boundary of unsorted subarray
+        for (int i = 0; i < n-1; i++)
+        {
+            // Find the minimum element in unsorted array
+            int minIndx = i;
+            for (int j = i+1; j < n; j++){
+
+            visualizer.setArr(arr);
+            visualizer.setComparedIndxes(i,j);
+            visualizer.repaint();
+
+            try {
+                Thread.sleep(delay);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+
+                if (arr[j] < arr[minIndx])
+                    minIndx = j;    
+        }
+            int temp = arr[minIndx];
+            arr[minIndx] = arr[i];
+            arr[i] = temp;
+            
+            //redraw to visualize change
+            visualizer.setArr(arr);
+            visualizer.repaint();
+            try {
+                Thread.sleep(delay);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+
     }
 }
--- driver.java
+++ driver.java
@@ -1,10 +1,35 @@
 
 
-public class driver{
-    public static void main(String[] args){
-        
-        System.out.println("Hello world!");
+import javax.swing.*;
+import java.util.Random;
+
+public class driver {
+
+    public static void main(String[] args) {
+        int[] array = generateRandomArray(100);
+
+        JFrame frame = new JFrame("Sorting Visualizer");
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setSize(1000, 600);
+
+        Visualizer visualizer = new Visualizer(array);
+        frame.add(visualizer);
+
+        frame.setVisible(true);
+
+        //Algorithms.InsertionSort(array, 10, visualizer);
+        Algorithms.selectionSort(array, 10, visualizer);
+        //Algorithms.mergeSort(array, 10, visualizer);
     }
 
+    private static int[] generateRandomArray(int size) {
+        int[] array = new int[size];
+        Random random = new Random();
 
+        for (int i = 0; i < size; i++) {
+            array[i] = random.nextInt(100); // Generate random numbers between 0 and 99
+        }
+
+        return array;
+    }
 }
